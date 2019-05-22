925d3b12b686947bfad7c12912fc7ee9ebb008b3
Juan Pablo Arroyave <50724493+JuanPh2002@users.noreply.github.com>
2019-05-22 15:28:10 -0500

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,9 +1,25 @@
-const cursos = require ('./datoscursos');
+const cursos = require("./datoscursos");
 const aspirantes = require('./inscribir');
 
-
 const argv = require ('yargs')
-	.command('inscribir','Realizar proceso de inscripcion',aspirantes.opciones)
-    .argv;
+	            .command('inscribir','Realizar proceso de inscripcion',aspirantes.opciones)
+                .argv;
+
+if (typeof(argv.nombre) != 'undefined' ) {
+    if ( typeof(cursos.buscarCurso(argv.i)) == 'undefined' ) {
+        aspirantes.cursoErroneo();
+        cursos.descripcionCursos();
+        //process.exit();
+    } else {
+        cursos.describirCurso(cursos.buscarCurso(argv.i));
+        aspirantes.crearArchivo(argv.n, argv.c, cursos.buscarCurso(argv.i));
+    }
     
+} else {
+    cursos.descripcionCursos();
+}      
+
+
+
+
 
